5382e53a58a119eb830b6768b5c69e4c49d29f41
fonxy6 <viktor_6@windowslive.com>
2024-02-07 10:35:21 +0100

    feature: chat/messages tests created, all passed.

--- Server/MessageAppServer/Model/Chat/Message.cs
+++ Server/MessageAppServer/Model/Chat/Message.cs
@@ -3,13 +3,22 @@ using System.Globalization;
 
 namespace Server.Model.Chat;
 
-public class Message(string roomId, string senderName, string text)
+public class Message
 {
     [Key]
     public string MessageId { get; set; } = Guid.NewGuid().ToString();
 
-    public string RoomId { get; set; } = roomId;
-    public string SenderName { get; set; } = senderName;
-    public string Text { get; set; } = text;
+    public string RoomId { get; set; } 
+    public string SenderName { get; set; } 
+    public string Text { get; set; } 
     public string SendTime { get; set; } = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+    
+    public Message() { }
+
+    public Message(string roomId, string senderName, string text)
+    {
+        RoomId = roomId;
+        SenderName = senderName;
+        Text = text;
+    }
 }
