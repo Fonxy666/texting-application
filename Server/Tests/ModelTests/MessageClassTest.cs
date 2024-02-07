7b288cd3086c7da366ff6cbb69d6a30de65260d5
fonxy6 <viktor_6@windowslive.com>
2024-02-07 08:08:11 +0100

    feature: model tests created, all passed.

---
+++ Server/Tests/ModelTests/MessageClassTest.cs
@@ -0,0 +1,24 @@
+﻿using Server.Model.Chat;
+
+namespace Tests.ModelTests
+{
+    [TestFixture]
+    public class MessageTests
+    {
+        [Test]
+        public void Message_Constructor_SetsPropertiesCorrectly()
+        {
+            var roomId = "TestRoomId";
+            var senderName = "TestSender";
+            var text = "TestText";
+
+            var message = new Message(roomId, senderName, text);
+
+            Assert.IsNotNull(message.MessageId);
+            Assert.AreEqual(roomId, message.RoomId);
+            Assert.AreEqual(senderName, message.SenderName);
+            Assert.AreEqual(text, message.Text);
+            Assert.IsNotNull(message.SendTime);
+        }
+    }
+}
