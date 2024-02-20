44d4352e545fe9199ce599b3a29210f93ad5925f
fonxy6 <viktor_6@windowslive.com>
2024-02-20 09:37:42 +0100

    feature: AuthController.cs got 100% code coverage with tests

--- Server/Tests/IntegrationTests/AuthControllerTests.cs
+++ Server/Tests/IntegrationTests/AuthControllerTests.cs
@@ -1,10 +1,13 @@
-﻿using System.Text;
+﻿using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using Server;
 using Server.Requests;
 using Server.Responses;
+using Server.Services.EmailSender;
 using Xunit;
+using Assert = Xunit.Assert;
 
 namespace Tests.IntegrationTests;
 
@@ -42,6 +45,26 @@ public class AuthControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     }
     
     [Fact]
+    public async Task Login_With_Invalid_User_ReturnBadRequestStatusCode()
+    {
+        var request = new AuthRequest("", "");
+        var authJsonRequest = JsonConvert.SerializeObject(request);
+        var authContent = new StringContent(authJsonRequest, Encoding.UTF8, "application/json");
+        var authResponse = await _client.PostAsync("/Auth/Login", authContent);
+        Assert.Equal(HttpStatusCode.BadRequest, authResponse.StatusCode);
+    }
+    
+    [Fact]
+    public async Task Login_With_Bad_Credentials_ReturnNotFoundStatusCode()
+    {
+        var request = new AuthRequest("TestUsername1", "testUserPassword123");
+        var authJsonRequest = JsonConvert.SerializeObject(request);
+        var authContent = new StringContent(authJsonRequest, Encoding.UTF8, "application/json");
+        var authResponse = await _client.PostAsync("/Auth/Login", authContent);
+        Assert.Equal(HttpStatusCode.NotFound, authResponse.StatusCode);
+    }
+    
+    [Fact]
     public async Task Register_Test_User_ReturnSuccessStatusCode()
     {
         var testUser = new RegistrationRequest("unique@hotmail.com", "uniqueTestUsername", "TestUserPassword", "01234567890", "");
@@ -50,6 +73,17 @@ public class AuthControllerTests : IClassFixture<WebApplicationFactory<Startup>>
 
         var getUserResponse = await _client.PostAsync("/Auth/Register", userLogin);
         getUserResponse.EnsureSuccessStatusCode();
+    }
+    
+    [Fact]
+    public async Task Register_Invalid_Test_User_ReturnBadRequest()
+    {
+        var testUser = new RegistrationRequest("", "", "", "", "");
+        var jsonLoginRequest = JsonConvert.SerializeObject(testUser);
+        var userLogin = new StringContent(jsonLoginRequest, Encoding.UTF8, "application/json");
+
+        var getUserResponse = await _client.PostAsync("/Auth/Register", userLogin);
+        Assert.Equal(HttpStatusCode.BadRequest, getUserResponse.StatusCode);
     }
     
     [Fact]
@@ -67,5 +101,43 @@ public class AuthControllerTests : IClassFixture<WebApplicationFactory<Startup>>
 
         var getUserResponse = await _client.DeleteAsync(deleteUrl);
         getUserResponse.EnsureSuccessStatusCode();
+    }
+    
+    [Fact]
+    public async Task VerifyToken_Invalid_Code_ReturnsBadRequest()
+    {
+        var testRequest = new VerifyTokenRequest("test@email.com", "invalid-code");
+        var jsonRequest = JsonConvert.SerializeObject(testRequest);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/Auth/ExamineVerifyToken", content);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task VerifyToken_Valid_Code_ReturnsOk()
+    {
+        var validCode = EmailSenderCodeGenerator.GenerateToken("test@email.com");
+
+        var testRequest = new VerifyTokenRequest("test@email.com", validCode);
+        var jsonRequest = JsonConvert.SerializeObject(testRequest);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/Auth/ExamineVerifyToken", content);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+    
+    [Fact]
+    public async Task SendEmailVerificationCode_ValidRequest_ReturnsOk()
+    {
+        var emailRequest = new GetEmailForVerificationRequest("test@test.hu");
+        var jsonRequest = JsonConvert.SerializeObject(emailRequest);
+        var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+        var response = await _client.PostAsync("/Auth/GetEmailVerificationToken", content);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
     }
 }
