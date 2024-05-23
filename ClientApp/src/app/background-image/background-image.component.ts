6efcc6f426463eda3b75b153a149138757ef0096
fonxy6 <viktor_6@windowslive.com>
2024-05-23 15:38:11 +0200

    feature: login/registration page ui updated

--- ClientApp/src/app/background-image/background-image.component.ts
+++ ClientApp/src/app/background-image/background-image.component.ts
@@ -6,7 +6,7 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './background-image.component.html',
   styleUrls: ['./background-image.component.css']
 })
-export class BackgroundImageComponent implements OnInit, AfterViewInit {
+export class BackgroundImageComponent implements AfterViewInit {
     @ViewChild('backgroundVideo') backgroundVideo!: ElementRef<HTMLVideoElement>;
     backgroundVideoSrc: string = "./assets/videos/white_black_video.mp4";
     animation: boolean;
@@ -15,41 +15,14 @@ export class BackgroundImageComponent implements OnInit, AfterViewInit {
         this.animation = this.cookiesService.get("Animation") == "True";
     }
 
-    ngOnInit() {
-        console.log(this.animation);
-    }
-
     ngAfterViewInit() {
         setTimeout(() => {
             const video = this.backgroundVideo?.nativeElement;
             if (video) {
                 this.renderer.setProperty(video, 'playbackRate', 1.5);
-                const playPromise = video.play();
-                
-                if (playPromise !== undefined) {
-                    playPromise.catch((error) => {
-                        console.error('Video play prevented:', error);
-                        this.setupUserInteraction(video);
-                    });
-                }
             } else {
                 console.error('Background video element not found');
             }
         }, 0);
-    }
-
-    private setupUserInteraction(video: HTMLVideoElement) {
-        const userInteractionHandler = () => {
-            video.play().then(() => {
-                console.log('Video playing after user interaction');
-            }).catch((error) => {
-                console.error('Video play failed after user interaction:', error);
-            });
-            document.removeEventListener('click', userInteractionHandler);
-            document.removeEventListener('keydown', userInteractionHandler);
-        };
-
-        document.addEventListener('click', userInteractionHandler);
-        document.addEventListener('keydown', userInteractionHandler);
     }
 }
