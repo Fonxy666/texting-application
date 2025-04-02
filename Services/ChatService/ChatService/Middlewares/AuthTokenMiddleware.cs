59b20b7b0beeef86c230c7564b3f179ddcaa544c
fonxy6 <viktor_6@windowslive.com>
2025-04-02 15:04:20 +0200

    feature: integrated the new middleware package to the chatservice as well

--- Services/ChatService/ChatService/Middlewares/AuthTokenMiddleware.cs
+++ Services/ChatService/ChatService/Middlewares/AuthTokenMiddleware.cs
@@ -1,11 +1,22 @@
 ﻿using System.Security.Claims;
+using JwtRefreshMiddlewareLibrary;
 
 namespace ChatService.Middlewares;
 
-public class AuthTokenMiddleware(RequestDelegate next, GrpcUserService.GrpcUserServiceClient grpcClient)
+public class AuthTokenMiddleware(RequestDelegate next, GrpcUserService.GrpcUserServiceClient grpcClient, JwtRefreshTokenMiddleware jwtMiddleware)
 {
     public async Task InvokeAsync(HttpContext context)
     {
+        if (jwtMiddleware.ExamineCookies(context))
+        {
+            // need new jwt token
+        }
+
+        if (jwtMiddleware.TokenExpired(context))
+        {
+            // need new refresh token
+        }
+
         var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
         if (userId == null)
         {
