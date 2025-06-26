9af772009e1d43573661de4fe505465325d021ad
fonxy6 <viktor_6@windowslive.com>
2025-06-26 19:12:27 +0200

    feature: started to write configure for both chatservice classes, for messages and for rooms as well.

---
+++ Services/ChatService/ChatService/Database/EntityConfigurations/MessageConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ChatService.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatService.Database.EntityConfigurations;
+
+public class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.HasKey(m => m.MessageId);
+
+        builder.HasOne(m => m.Room)
+            .WithMany(m => m.Messages)
+            .HasForeignKey(m => m.RoomId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Services/ChatService/ChatService/Database/EntityConfigurations/RoomConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using ChatService.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatService.Database.EntityConfigurations;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder.HasKey(r => r.RoomId);
+        
+        builder.Property(r => r.RoomId)
+            .IsRequired();
+        
+        builder.Property(r => r.CreatorId)
+            .IsRequired();
+        
+        builder.Property(r => r.RoomName)
+            .IsRequired();
+        
+        builder.Property(r => r.Password)
+            .IsRequired()
+            .HasMaxLength(1024);
+        
+        
+    }
+}
