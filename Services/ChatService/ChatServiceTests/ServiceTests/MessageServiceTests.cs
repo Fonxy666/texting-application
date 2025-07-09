c6f07aa961761a055d06720ab89174977ab8a676
fonxy6 <viktor_6@windowslive.com>
2025-07-09 09:19:11 +0200

    feature: created chatservicetests skeleton, starting to write the tests.

---
+++ Services/ChatService/ChatServiceTests/ServiceTests/MessageServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace ChatServiceTests.ServiceTests;
+
+public class MessageServiceTests : IAsyncLifetime
+{
+    public Task InitializeAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DisposeAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
