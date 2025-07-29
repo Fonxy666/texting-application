aec1512a9deb673591abd283b00618e2d3f4712a
fonxy6 <viktor_6@windowslive.com>
2025-07-29 20:28:31 +0200

    test:test

--- Services/ChatService/ChatServiceTests/IntegrationTests/ChatControllerTests.cs
+++ Services/ChatService/ChatServiceTests/IntegrationTests/ChatControllerTests.cs
@@ -46,7 +46,7 @@ public class ChatControllerTests : IClassFixture<WebApplicationFactory<Startup>>
             .AddConfiguration(baseConfig)
             .AddInMemoryCollection(new Dictionary<string, string>
             {
-                { "ConnectionStrings:DefaultConnection", _testConnectionString }
+                { "ConnectionStrings:DefaultConnection", "Host=localhost;Port=5435;Username=postgres;Password=testPassword123@;Database=test_chat_db" }
             }!)
             .Build();
 
