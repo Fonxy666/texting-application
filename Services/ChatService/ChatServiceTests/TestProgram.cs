8bfcece4ec698777a85e74c6dbc605b1cd9bd6ba
fonxy6 <viktor_6@windowslive.com>
2025-07-29 19:30:41 +0200

    feature: added test program.cs.

---
+++ Services/ChatService/ChatServiceTests/TestProgram.cs
@@ -0,0 +1,13 @@
+﻿using ChatServiceTests;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+CreateHostBuilder(args).Build().Run();
+return;
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<TestStartup>();
+        });
